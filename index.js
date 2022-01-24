648f65c5219846fbe44ecdbb63742a267f0efb10
foysal <foysalf652@gmail.com>
2022-01-24 20:39:53 +0600

    user connected and disconected

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+const { time } = require('console');
+const { TIMEOUT } = require('dns');
 const express=require('express');
 const app=express();
 const http=require('http');
 const { hostname } = require('os');
 const expressServer = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+
 
 const { Server } = require('socket.io');
 const io = new Server(expressServer);
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
-expressServer.listen(3000, () => { 
-  
-  console.log('listening on *:3000'); 
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+expressServer.listen(5000, () => {
+  console.log('serverRun  *:5000');
 });
